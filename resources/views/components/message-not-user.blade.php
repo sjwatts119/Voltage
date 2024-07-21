8e66e48b41ccbffeb54420b9689a5f92a3fb57b8
sjwatts119 <samuel.watts67@gmail.com>
2024-07-21 16:35:11 +0100

    Timestamps and Text Wrapping
    Needs better date logic but pushing that back for now

--- resources/views/components/message-not-user.blade.php
+++ resources/views/components/message-not-user.blade.php
@@ -1,8 +1,11 @@
-<div class="col-start-1 col-end-12 lg:col-end-10 p-3 rounded-lg">
+<div class="col-start-1 col-end-11 lg:col-end-9 p-3 rounded-lg">
     <div class="flex flex-row items-center">
         <img src="https://placehold.jp/150x150.png" alt="" class="flex items-center justify-center h-10 w-10 rounded-full flex-shrink-0"/>
         <div class="relative ml-3 text-sm bg-white dark:bg-slate-700 dark:text-slate-100 py-2 px-4 shadow rounded-xl">
-            <div>{{$message['message']}}</div>
+            <div class="break-words">{{$message['message']}}</div>
+        </div>
+        <div class="relative ml-3 text-xs dark:text-slate-400 py-2">
+            <div>{{date('H:i' , $message['time'])}}</div>
         </div>
     </div>
 </div>
--- resources/views/components/message-user.blade.php
+++ resources/views/components/message-user.blade.php
@@ -2,7 +2,10 @@
     <div class="flex items-center justify-start flex-row-reverse">
         <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500  flex-shrink-0">A</div>
         <div class="relative mr-3 text-sm bg-indigo-100 dark:bg-indigo-500 dark:text-slate-100 py-2 px-4 shadow rounded-xl">
-            <div>{{$message['message']}}</div>
+            <div class="break-words">{{$message['message']}}</div>
+        </div>
+        <div class="relative mr-3 text-xs dark:text-slate-400 py-2">
+            <div>{{date('H:i' , $message['time'])}}</div>
         </div>
     </div>
 </div>
