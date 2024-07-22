3934d77d179837c67e453fbc20ffc9b9ca6cdbb7
sjwatts119 <samuel.watts67@gmail.com>
2024-07-22 23:44:53 +0100

    Hide emoji on mobile

--- resources/views/components/chat-area-inputs.blade.php
+++ resources/views/components/chat-area-inputs.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row items-center h-16 py-7 bg-white dark:bg-gray-800 w-full px-3 sm:px-10">
+<div class="flex flex-row items-center h-16 py-7 bg-white dark:bg-gray-800 w-full px-4 md:px-10">
     <div>
         <button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -9,7 +9,7 @@
     <div class="flex-grow ml-4">
         <div class="relative w-full">
             <input wire:model="messageInput" wire:keydown.enter="sendMessage()" type="text" class="flex w-full border dark:border-gray-700 rounded-xl focus:outline-none focus:border-indigo-300 dark:bg-gray-900 pl-4 h-10 transition dark:text-gray-100" placeholder="Start typing..."/>
-            <button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+            <button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600 hidden sm:block">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
