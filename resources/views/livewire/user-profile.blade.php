0a125604c66be20e5b2f3a25698eed0a63f700be
sjwatts119 <samuel.watts67@gmail.com>
2024-07-23 05:19:49 +0100

    Default Banner Image if none exist
    no banner images exist right now, in fact no images exist at all, so the condition for whether or not one exists will have to change when it is properly implemented as it likely will need to use a spatie function to get the URL

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -1,7 +1,12 @@
 <div class="container rounded-2xl">
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl">
-        <div class="h-32 sm:h-52 overflow-hidden" >
-            <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+        <div class="h-32 sm:h-52 overflow-hidden">
+            {{--this will be done with spatie so this isn't correct but it's just a placeholder for now--}}
+            @if($user->profile->banner)
+                <img class="w-full" src="" alt="" />
+            @else
+                <div class="bg-gradient-to-r from-purple-500 to-indigo-500 w-full h-full"></div>
+            @endif
         </div>
         <div class="flex justify-center px-5 -mt-12">
             <x-profile-icon :user="$user" />
@@ -11,10 +16,22 @@
                 <h2 class="text-gray-800 dark:text-gray-200 text-4xl font-bold">{{  $user->name }}</h2>
                 <div class="flex items-start">
                     <a class="text-gray-400 text-lg mt-1 hover:text-purple-500 mr-1">{{ $user->username }}</a>
-                    <span class="text-gray-400 text-lg">.</span>
-                    <p class="text-gray-400 text-lg mt-1 ml-1">{{ $user->profile->pronouns }}</p>
+                    @if($user->profile->pronouns)
+                        <span class="text-gray-400 text-lg">.</span>
+                        <p class="text-gray-400 text-lg mt-1 ml-1">{{ $user->profile->pronouns }}</p>
+                    @endif
                 </div>
-                <p class="mt-2 text-gray-500 text-md mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+
+                {{--page divider--}}
+                <div class="border-t border-gray-100 dark:border-gray-700 my-4"></div>
+
+                <p class="mt-2 text-gray-500 text-md mt-4">
+                    @if($user->profile->bio)
+                        {{ $user->profile->bio }}
+                    @else
+                        This user has not set a bio.
+                    @endif
+                </p>
             </div>
         </div>
     </div>
