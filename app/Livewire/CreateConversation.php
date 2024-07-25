8fe4d80631123c1b8cc17b38ca12861ca6bfdb01
sjwatts119 <samuel.watts67@gmail.com>
2024-07-25 07:45:57 +0100

    Basic User Searching
    Livewire is so insanely good for this.

--- app/Livewire/CreateConversation.php
+++ app/Livewire/CreateConversation.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use LivewireUI\Modal\ModalComponent;
 
 class CreateConversation extends ModalComponent
 {
+    public $search = '';
+
     public function render()
     {
-        return view('livewire.create-conversation');
+        $results = [];
+
+        if(strlen($this->search) > 0) {
+            $results = User::where('name', 'like', '%' . $this->search . '%')->limit(10)->get();
+        }
+
+        return view('livewire.create-conversation', [
+            'results' => $results,
+        ]);
     }
 }
--- resources/views/livewire/create-conversation.blade.php
+++ resources/views/livewire/create-conversation.blade.php
@@ -1,3 +1,31 @@
 <div class="container rounded-2xl overflow-auto max-w-6xl h-[40dvh] dark:bg-gray-800 bg-white">
 
+    {{-- Search Bar --}}
+    <div class="flex items-center justify-between p-4 border-b dark:border-gray-700">
+        <input wire:model.live="search" type="text" class="w-full px-4 py-2 text-sm dark:bg-gray-800 dark:text-gray-200 rounded-lg" placeholder="Search for users...">
+    </div>
+
+    @foreach($results as $user)
+        <div class="flex items-center justify-between p-4 border-b dark:border-gray-700">
+            <div class="flex items-center space-x-4">
+
+                @if($user->profile->profile_photo)
+                    <img src="{{ asset('storage/' . $user->profile->profile_photo) }}" alt="User Image" class="w-12 h-12 rounded-full object-cover">
+                @else
+                    <div class="w-12 h-12 bg-purple-400 rounded-full flex items-center justify-center">
+                        <div class="text-2xl font-sans text-gray-800">{{ $user->name[0] }}</div>
+                    </div>
+                @endif
+
+                <div>
+                    <div class="text-lg font-semibold dark:text-gray-200">{{ $user->name }}</div>
+                    <div class="text-sm dark:text-gray-300">{{ $user->email }}</div>
+                </div>
+            </div>
+            <button wire:click="createConversation({{ $user->id }})" class="flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-lg">
+                Message
+            </button>
+        </div>
+        @endforeach
+
 </div>
