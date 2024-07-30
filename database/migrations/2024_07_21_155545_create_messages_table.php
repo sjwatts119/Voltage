5b98786f848dec635d94c68edd76f9508429af05
sjwatts119 <samuel.watts67@gmail.com>
2024-07-30 15:20:18 +0100

    System Messages in Migration
    Preparing for the ability to send system messages.
    
    Relates to #3

--- database/migrations/2024_07_21_155545_create_messages_table.php
+++ database/migrations/2024_07_21_155545_create_messages_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->enum('type', ['user', 'system'])->default('user');
             $table->text('message');
             $table->timestamps();
         });
+
     }
 
     /**
