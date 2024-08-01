6bbf3d25bfec881b168fc9642cdf2a6896baf11f
sjwatts119 <samuel.watts67@gmail.com>
2024-08-01 19:57:01 +0100

    Remove Emoji Button
    Relates to #7

--- resources/views/components/chat-area-inputs.blade.php
+++ resources/views/components/chat-area-inputs.blade.php
@@ -9,11 +9,6 @@
     <div class="flex-grow ml-4">
         <div class="relative w-full">
             <input wire:model="messageInput" wire:keydown.enter="sendMessage()" type="text" class="flex w-full border dark:border-gray-700 rounded-xl focus:outline-none focus:border-indigo-300 dark:bg-gray-900 pl-4 h-10 transition dark:text-gray-100 transition" placeholder="Start typing..."/>
-            <button class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600 hidden sm:block transition">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                </svg>
-            </button>
         </div>
     </div>
     <div class="ml-4">
