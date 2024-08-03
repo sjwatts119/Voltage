9bfbaf484558217225f17886d478dd17254cc89a
sjwatts119 <samuel.watts67@gmail.com>
2024-08-03 13:42:25 +0100

    Temporarily Remove Delete Account
    I don't have the time at the moment to properly implement deleted users, so pushing back any changes and removing the ability for a single user to completely break the site.
    
    Relates to #13

--- resources/views/components/settings-account.blade.php
+++ resources/views/components/settings-account.blade.php
@@ -13,10 +13,12 @@
             </div>
         </div>
 
+        {{--
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow rounded-lg">
             <div class="max-w-xl">
                 <livewire:profile.delete-user-form />
             </div>
         </div>
+        --}}
     </div>
 </div>
