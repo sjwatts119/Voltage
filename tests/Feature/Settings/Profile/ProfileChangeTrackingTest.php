a993558a7a6128a8e5d5a15c2c343ccab5a1c12e
sjwatts119 <48589467+sjwatts119@users.noreply.github.com>
2024-08-16 21:30:25 +0100

    Testing for Discarding of Changes

--- tests/Feature/Settings/Profile/ProfileChangeTrackingTest.php
+++ tests/Feature/Settings/Profile/ProfileChangeTrackingTest.php
@@ -120,4 +120,52 @@ class ProfileChangeTrackingTest extends TestCase
         // Assert that changes are detected
         $this->assertTrue($hasChanges);
     }
+
+    public function test_all_changes_detected_when_all_fields_are_changed(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        // Test Livewire component
+        $component = Livewire::test(SettingsProfile::class)
+            ->set('user', $user)
+            ->set('name', 'New Name!')
+            ->set('pronouns', 'they/them')
+            ->set('bio', 'New bio!')
+            ->set('profilePicture', UploadedFile::fake()->image('newprofilepicture.jpg')->size(800))
+            ->set('bannerPicture', UploadedFile::fake()->image('newbannerpicture.jpg')->size(2000));
+
+        // Call hasChanges and capture its result
+        $hasChanges = $component->instance()->hasChanges();
+
+        // Assert that all changes are detected
+        $this->assertTrue($hasChanges);
+    }
+
+    public function test_discard_changes_resets_to_default_values(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        // Test Livewire component
+        $component = Livewire::test(SettingsProfile::class)
+            ->set('user', $user)
+            ->set('name', 'New Name!')
+            ->set('pronouns', 'they/them')
+            ->set('bio', 'New bio!')
+            ->set('profilePicture', UploadedFile::fake()->image('newprofilepicture.jpg')->size(800))
+            ->set('bannerPicture', UploadedFile::fake()->image('newbannerpicture.jpg')->size(2000));
+
+        // Call discardChanges
+        $component->call('discardChanges');
+
+        // Assert that all values are reset to default
+        $this->assertEquals($user->name, $component->name);
+        $this->assertEquals($user->profile->pronouns, $component->pronouns);
+        $this->assertEquals($user->profile->bio, $component->bio);
+        $this->assertNull($component->profilePicture);
+        $this->assertNull($component->bannerPicture);
+    }
 }
