b829ebeb7aca2baaf96f056e6f294db3c02e69ad
sjwatts119 <48589467+sjwatts119@users.noreply.github.com>
2024-08-18 16:46:53 +0100

    Include Flowbite as NPM package instead of through CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
     </head>
     {{--get the dark mode setting from the user's settings and set it on load--}}
     {{--also listen for the dark-mode and light-mode events to change the dark mode setting dynamically--}}
