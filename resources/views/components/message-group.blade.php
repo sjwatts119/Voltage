08c28f2b1623f7e3a9b6e33814975b5881bc121e
sjwatts119 <samuel.watts67@gmail.com>
2024-08-21 18:56:27 +0100

    Variety of fixes and improvements
    Icon for Edit button on popup menu, matches delete button nicely.
    
    Moved edit message input to a component for reuse.
    
    Up arrow to edit last user message requires the user to be focused on the main chat area input, as some users may use the up arrows to scroll up the chat and this reduces accessibility if not done.

--- resources/views/components/message-group.blade.php
+++ resources/views/components/message-group.blade.php
@@ -27,17 +27,7 @@
                 </div>
                 {{--for the first message include it on the same row as the user info--}}
                 @if($currentlyEditingId && $currentlyEditingId === $messageGroup[0]->id)
-                    <form wire:submit.prevent="updateMessage($event.target.elements.message.value)">
-                        <div class="flex items-center space-x-2 my-1">
-                            <input name="message" type="text" class="text-sm w-full dark:bg-gray-800 dark:text-white rounded-lg p-1 px-2 focus:outline-none" placeholder="Edit your message..." value="{{$messageGroup[0]->message}}">
-                            <button type="submit" class="text-sm text-white rounded-lg focus:outline-none">
-                                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5"/>
-                                </svg>
-                            </button>
-                            <button wire:click="cancelEditingMessage()" type="button" class="text-md dark:text-white rounded-lg focus:outline-none">X</button>
-                        </div>
-                    </form>
+                    <x-edit-message-input :message="$messageGroup[0]"/>
                 @else
                     <div class="text-sm dark:text-slate-300 py-1 break-all relative group">
                         {{ $messageGroup[0]->message }}
@@ -79,35 +69,7 @@
             {{-- Message --}}
             <div class="text-sm dark:text-slate-300 py-1 break-all relative group flex-grow">
                 @if($currentlyEditingId && $currentlyEditingId === $message->id)
-                    <form wire:submit.prevent="updateMessage($event.target.elements.message.value)"
-                          x-data
-                          x-init="$nextTick(() => {
-                              const input = $refs.messageInput;
-                              input.focus();
-                              input.setSelectionRange(input.value.length, input.value.length);
-                          })"
-                          @keydown.window.escape="$dispatch('cancel-editing-message')">
-                        <div class="flex items-center space-x-2 my-1">
-                            <input
-                                x-ref="messageInput"
-                                name="message"
-                                type="text"
-                                class="text-sm w-full dark:bg-gray-800 dark:text-white rounded-lg p-1 px-2 focus:outline-none"
-                                placeholder="Edit your message..."
-                                value="{{$message->message}}"
-                                @keydown.escape.window="$dispatch('cancel-editing-message')"
-                            >
-                            <button type="submit" class="text-sm text-white rounded-lg focus:outline-none">
-                                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5"/>
-                                </svg>
-                            </button>
-                            <button wire:click="cancelEditingMessage()" type="button" class="text-md dark:text-white rounded-lg focus:outline-none">X</button>
-                        </div>
-                    </form>
-
-
-
+                    <x-edit-message-input :message="$message"/>
                 @else
                     <div>
                         {{ $message->message }}
