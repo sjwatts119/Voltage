5dc159e5be7bc476b9f2b0de03217b82cd976c95
sjwatts119 <samuel.watts67@gmail.com>
2024-08-23 23:28:19 +0100

    Displaying multiple different file types in one message

--- resources/views/components/view-attachments-file.blade.php
+++ resources/views/components/view-attachments-file.blade.php
@@ -1,5 +1,5 @@
-<div class="flex items-center space-x-4 p-4 rounded-lg bg-gray-100 dark:bg-gray-800">
-    <div class="flex flex-col items-center">
+<div class="flex items-center space-x-4 p-4 rounded-lg bg-gray-100 dark:bg-gray-800 my-1 max-w-lg">
+    <div class="flex flex-col items-center ">
         <span class="text-xs font-semibold text-gray-600 dark:text-gray-300 uppercase">{{ pathinfo($attachment->attachment_path, PATHINFO_EXTENSION) }}</span>
         <svg class="w-8 h-8 text-gray-600 dark:text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6H6c-1.1 0-2 .9-2 2z"/>
@@ -8,7 +8,7 @@
     </div>
 
     <div class="flex-1">
-        <a href="{{ asset('storage/attachments/' . $attachment->attachment_path) }}" target="_blank" class="block text-sm text-gray-900 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-300 truncate">
+        <a href="{{ asset('storage/attachments/' . $attachment->attachment_path) }}" target="_blank" class="block text-sm text-gray-900 dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-300 truncate max-w-md">
             {{ basename($attachment->attachment_path) }}
         </a>
         <span class="text-xs text-gray-500 dark:text-gray-400">
--- resources/views/components/view-attachments.blade.php
+++ resources/views/components/view-attachments.blade.php
@@ -2,14 +2,19 @@
     @if($attachments->every(fn($attachment) => str_contains($attachment->type, 'image')))
         {{-- Is there only one attachment? Show a single image--}}
         @if($attachments->count() === 1)
-            <x-view-attachments-image :attachments="$attachments"/>
+            <x-view-attachments-image :attachment="$attachments[0]"/>
         {{-- Are there two or more attachments? Show a gallery of images --}}
         @elseif($attachments->count() > 1)
             <x-view-attachments-gallery :attachments="$attachments"/>
         @endif
     @else
         @foreach($attachments as $attachment)
-            <x-view-attachment-file :attachment="$attachment" :loop="$loop"/>
+            {{-- If the attachments is an image, display the image itself and not the file preview --}}
+            @if(str_contains($attachment->type, 'image'))
+                <x-view-attachments-image :attachment="$attachment"/>
+            @else
+                <x-view-attachments-file :attachment="$attachment" :loop="$loop"/>
+            @endif
         @endforeach
     @endif
 @endif
