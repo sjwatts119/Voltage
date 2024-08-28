8cac68b167337b75bec523161a87dbc5c4a8a907
Sam Watts <samuel.watts67@gmail.com>
2024-08-28 20:31:25 +0100

    Fix single images with messages not displaying properly

--- resources/views/components/view-attachments-image.blade.php
+++ resources/views/components/view-attachments-image.blade.php
@@ -1,4 +1,4 @@
-<div class="relative mt-2 inline-block">
+<div class="relative mt-2">
     <img src="{{ asset('storage/attachments/' . $attachment->message->id. '/thumbnail-' . $attachment->attachment_path) }}"
          alt="An image sent by {{ $attachment->message->user->name }}"
          class="rounded-lg h-[300px] object-cover cursor-pointer transition-transform duration-300 hover:scale-105"
