4f56cef3efe71bd07eb13c7ba0f6b1e82b4a99fb
Sam Watts <samuel.watts67@gmail.com>
2024-08-29 20:39:00 +0100

    Final Touchups

--- resources/views/livewire/message-attachment.blade.php
+++ resources/views/livewire/message-attachment.blade.php
@@ -15,7 +15,7 @@
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                         </svg>
                         <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">Any file (Max 10MB)</p>
                     </div>
                     <input wire:model="newAttachments" id="dropzone-file" type="file" multiple="multiple" class="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"/>
                 </label>
